fa7caf3f1b382cba6d1a247514f60a946152d5c6
Happy Kids <loomakinigor311sp@gmail.com>
2026-06-24 19:59:21 -0300

    Add products array with sample product data

---
+++ products.js
@@ -0,0 +1,23 @@
+const products = [
+  {
+    id: 1,
+    name: "Машинка",
+    price: 200,
+    desc: "Яскрава дитяча машинка",
+    img: "https://via.placeholder.com/150"
+  },
+  {
+    id: 2,
+    name: "Лялька",
+    price: 350,
+    desc: "Красива лялька для дітей",
+    img: "https://via.placeholder.com/150"
+  },
+  {
+    id: 3,
+    name: "Конструктор",
+    price: 500,
+    desc: "Розвивальний набір",
+    img: "https://via.placeholder.com/150"
+  }
+];
